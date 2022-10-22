3520db817eee7b50df33e4f026e6c38637292b53
Mikael <mikael.srodrigues@gmail.com>
2022-10-21 22:21:14 -0300

    Update listaDeTarefas.js
    Aula de criação de filtro e localStorage

--- appListadeTarefas/listaDeTarefas.js
+++ appListadeTarefas/listaDeTarefas.js
@@ -6,6 +6,8 @@ let filtro = document.querySelector("#filtro");
 
 function carregaMonitoresEventos() {
 	btnAddTarefa.addEventListener("click", adicionarTarefa);
+	btnLimpar.addEventListener("click", removeTudo);
+	filtro.addEventListener("keyup", filtrarLista);
 
 	// Evento para fazer o x funcionar
 	lista.addEventListener("click", apagarTarefa);
@@ -27,24 +29,64 @@ function adicionarTarefa(evento) {
 
 	if (entradaTarefa.value === "") {
 		alert("Entre com uma tarefa");
+	} else {
+		// Cria novo Li com a nova tarefa
+		const li = document.createElement("li");
+		li.className = "collection-item";
+		li.appendChild(document.createTextNode(entradaTarefa.value));
+
+		// Cria o X que fecha a tarefa
+		const a = document.createElement("a");
+		a.className = "apaga-tarefa secondary-content";
+
+		const i = document.createElement("i");
+		i.className = "fa fa-remove";
+
+		a.appendChild(i);
+		li.appendChild(a);
+		lista.appendChild(li);
+
+		// Insere a tarefa no Local Storage
+		gravarTarefa(entradaTarefa.value);
+
+		// Limpa o campo de entrada
+		entradaTarefa.value = "";
+	}
+}
+
+function removeTudo(evento) {
+	evento.preventDefault();
+
+	lista.remove();
+}
+
+// Pegar caracter por carcter
+function filtrarLista(evento) {
+	const procurado = evento.target.value.toLowerCase();
+	const liItem = document.querySelectorAll(".collection-item");
+
+	liItem.forEach(function (tarefa) {
+		let textoTarefa = tarefa.innerText;
+
+		if (textoTarefa.toLowerCase().indexOf(procurado) != -1) {
+			tarefa.style.display = "block";
+		} else {
+			tarefa.style.display = "none";
+		}
+	});
+}
+
+function gravarTarefa(tarefa) {
+	let tarefas = [];
+	let tarefaLStorage = localStorage.getItem("tarefas");
+
+	if (tarefaLStorage != null) {
+		tarefas = JSON.parse(tarefaLStorage);
 	}
 
-	// Cria novo Li com a nova tarefa
-	const li = document.createElement("li");
-	li.className = "collection-item";
-	li.appendChild(document.createTextNode(entradaTarefa.value));
+	tarefas.push(tarefa);
+	localStorage.setItem("tarefas", JSON.stringify(tarefas));
+	lista = localStorage.getItem("tarefas");
 
-	// Cria o X que fecha a tarefa
-	const a = document.createElement("a");
-	a.className = "apaga-tarefa secondary-content";
-
-	const i = document.createElement("i");
-	i.className = "fa fa-remove";
-
-	a.appendChild(i);
-	li.appendChild(a);
-	lista.appendChild(li);
-
-	// Limpa o campo de entrada
-	entradaTarefa.value = "";
+	// lista.innerText = localStorage.getItem("tarefa");
 }
