2d50261893ede9e62e81d2c96e7529596643ea04
Mikael <mikael.srodrigues@gmail.com>
2022-11-18 22:09:15 -0300

    IPCA Terminado
    Conclusão do IPCA usando a API do banco central

--- graficos/ipca.js
+++ graficos/ipca.js
@@ -1,7 +1,11 @@
 document.querySelector("#btnIPCA").addEventListener("click", mostrarIPCA);
+let ipca = document.querySelector("#ipca");
 
 function mostrarIPCA(evento) {
 	evento.preventDefault();
+	ipca.style.display = "block";
+
+	document.querySelector("#graficoLinha").innerHTML = "";	
 
 	fetch(
 		"https://api.bcb.gov.br/dados/serie/bcdata.sgs.4448/dados?formato=json&dataInicial=20201101"
@@ -9,32 +13,35 @@ function mostrarIPCA(evento) {
 		.then(function (retorno) {
 			return retorno.text();
 		})
-		.then(function (conteudo) {
-			let grafico = JSON.parse(conteudo);
+		.then(function (cont) {
+			let conteudo = JSON.parse(cont);
+			let novobj = [];
+			let anoUser = document.querySelector("#input").value;
+
+			conteudo.forEach(function (mes) {
+				anoStr = mes.data.substring(6, 10);
+				mesStr = mes.data.substring(3, 5);
+				anoMesStr = anoStr + "-" + mesStr;
+
+				if (parseInt(anoStr) != anoUser && anoUser != "") {
+					return;
+				}
+				indice = parseFloat(mes.valor);
+				novobj.push({ month: anoMesStr, value: indice });
+			});
+
+			if (novobj.length == 0) {
+				alert(`Não há dados para o ano ${anoUser}`);
+			}
 
 			JSONMorris = {
-				element: "ipca",
+				element: "graficoLinha",
 
-				data: [
-					{ year: "2008", value: 20 },
-					{ year: "2009", value: 10 },
-					{ year: "2010", value: 5 },
-					{ year: "2011", value: 5 },
-					{ year: "2012", value: 20 },
-				],
-
-				// O nome do eixo com valores de X
-				xkey: "year",
-
-				// A lista de nome dos atributos do dados contidos em Y
+				data: novobj,
+				xkey: "month",
 				ykeys: ["value"],
-
-				//Rótuo para os índices - Aparece quando passa o mouse por cima
-				labels: ["Value"],
+				labels: ["IPCA"],
 			};
 			new Morris.Line(JSONMorris);
-
-			// let saida = grafico;
-			// document.querySelector(".ipca").innerHTML = saida;
 		});
 }
